c3b1b4796b050c94171d5062dfee273cd11bbbb8
Konrad <konradmalenczak@gmail.com>
2024-01-29 14:13:54 +0100

    Added ChangeGreedy algorithm.

---
+++ CPP/ChangeGreedy.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> changeGreedy(int amount, int denominations[], int size_of_array){
+    vector<int> change;
+    for(int i = 0; i < size_of_array; i++){
+        while(denominations[i] <= amount){
+            change.push_back(denominations[i]);
+            amount -= denominations[i];
+        }
+    }
+    return change;
+}
+
+int main() {
+    int denominations[] = {500, 200, 100, 50, 20, 10, 5, 2, 1};
+    int size_of_array = sizeof(denominations) / sizeof(denominations[0]);
+    vector<int> result = changeGreedy(43, denominations, size_of_array);
+    for(int denomination : result){
+        cout << denomination << ", ";
+    }
+}
---
+++ Python/ChangeGreedy.py
@@ -0,0 +1,9 @@
+def change_greedy(amount: int, denominations: list) -> list:
+    change = []
+    for denomination in denominations:
+        while denomination <= amount:
+            change.append(denomination)
+            amount -= denomination
+    return change
+
+print(change_greedy(43, [500, 200, 100, 50, 20, 10, 5, 2, 1]))
