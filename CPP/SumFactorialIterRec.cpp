585663ace717576484b56226bf6b131213f0a5bb
Konrad <konradmalenczak@gmail.com>
2024-02-02 09:36:16 +0100

    Renamed SumFactorialIterRec CPP algorithm names.

--- CPP/SumFactorialIterRec.cpp
+++ CPP/SumFactorialIterRec.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int sum_iterative(int number){
+int sumIterative(int number){
     if(number < 0){
         return -1;
     }
@@ -13,17 +13,17 @@ int sum_iterative(int number){
     return total_sum;
 }
 
-int sum_recursive(int number){
+int sumRecursive(int number){
     if(number < 0){
         return -1;
     }
     if(number == 0){
         return 0;
     }
-    return number + sum_recursive(number - 1);
+    return number + sumRecursive(number - 1);
 }
 
-int factorial_iterative(int number){
+int factorialIterative(int number){
     if(number < 0){
         return -1;
     }
@@ -34,26 +34,26 @@ int factorial_iterative(int number){
     return result;
 }
 
-int factorial_recursive(int number){
+int factorialRecursive(int number){
     if(number < 0){
         return -1;
     }
     if(number <= 1){
         return 1;
     }
-    return number * factorial_recursive(number - 1);
+    return number * factorialRecursive(number - 1);
 }
 
 int main(int argc, char** argv) {
-	cout << sum_iterative(5) << endl;
-	cout << sum_iterative(10) << endl;
+	cout << sumIterative(5) << endl;
+	cout << sumIterative(10) << endl;
 	
-	cout << sum_recursive(5) << endl;
-	cout << sum_recursive(10) << endl;
+	cout << sumRecursive(5) << endl;
+	cout << sumRecursive(10) << endl;
 	
-	cout << factorial_iterative(5) << endl;
-	cout << factorial_iterative(10) << endl;
+	cout << factorialIterative(5) << endl;
+	cout << factorialIterative(10) << endl;
 	
-	cout << factorial_recursive(5) << endl;
-	cout << factorial_recursive(10) << endl;
+	cout << factorialRecursive(5) << endl;
+	cout << factorialRecursive(10) << endl;
 }
