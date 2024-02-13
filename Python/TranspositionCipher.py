29bf68a44f1161a1d85ea3fc2cc9f43333cb747f
Konrad <konradmalenczak@gmail.com>
2024-02-13 08:50:08 +0100

    Updated TranspositionCipher algorithm.

--- Python/TranspositionCipher.py
+++ Python/TranspositionCipher.py
@@ -1,8 +1,8 @@
-def transpose_cipher(text: str) -> str:
+def transposition_cipher(text: str) -> str:
     text_as_list = list(text)
     for i in range(0, len(text_as_list) - 1, 2):
         text_as_list[i], text_as_list[i + 1] = text_as_list[i + 1], text_as_list[i]
     return "".join(text_as_list)
 
-print(transpose_cipher("COMPUTER"))
-print(transpose_cipher("PHONE"))
+print(transposition_cipher("COMPUTER"))
+print(transposition_cipher("PHONE"))
