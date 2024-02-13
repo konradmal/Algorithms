bc8d2ce7b396e5e3f0934882f9983bee43c57754
Konrad <konradmalenczak@gmail.com>
2024-02-13 08:48:23 +0100

    Updated Hanoi algorithm.

--- CPP/Hanoi.cpp
+++ CPP/Hanoi.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-void Hanoi(int n, string A, string B, string C){
+void hanoi(int n, string A, string B, string C){
 	if(n > 0){
-		Hanoi(n-1, A, C, B);
+		hanoi(n-1, A, C, B);
 		cout << A << " -> " << C << endl;
-		Hanoi(n-1, B, A, C);
+		hanoi(n-1, B, A, C);
 	}
 }
 
 int main() {
-	Hanoi(3, "A", "B", "C");
-	Hanoi(4, "A", "B", "C");
+	hanoi(3, "A", "B", "C");
+	hanoi(4, "A", "B", "C");
 }
