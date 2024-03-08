6171b5168e38a7fa5c26731766abdb4564e48c53
Konrad <konradmalenczak@gmail.com>
2024-03-08 16:10:28 +0100

    Updated NarcissisticNumber algorithm.

--- Python/NarcissisticNumber.py
+++ Python/NarcissisticNumber.py
@@ -1,8 +1,12 @@
-def narcissistic_number(number):
-    digits = [int(d) for d in str(number)]
+def narcissistic_number(number: int) -> bool:
+    digits = []
+    original_number = number
+    while number > 0:
+        digits.append(number % 10)
+        number //= 10
     num_digits = len(digits)
-    sum_of_powers = sum(d ** num_digits for d in digits)
-    return number == sum_of_powers
+    sum_of_powers = sum(digit ** num_digits for digit in digits)
+    return original_number == sum_of_powers
 
 print(153,":",narcissistic_number(153))
 print(154,":",narcissistic_number(154))
