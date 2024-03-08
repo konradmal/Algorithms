dff12e3131fa814e23425dcb2df85fe49f58200a
Konrad <konradmalenczak@gmail.com>
2024-03-08 15:51:39 +0100

    Updated BeaufortCipher algorithm.

--- CPP/BeaufortCipher.cpp
+++ CPP/BeaufortCipher.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 
-string encrypt(string text, int k) {
+string beaufortCipher(string text, int k) {
     int n = text.length();
     string ciphertext = "";
     for (int i = 0; i < n; ++i) {
@@ -15,10 +15,10 @@ string encrypt(string text, int k) {
 int main() {
     string text = "CAT";
     cout << text << endl;
-    string encrypted_text = encrypt(text, 1);
+    string encrypted_text = beaufortCipher(text, 1);
     cout << encrypted_text << endl;
     string text2 = "MOUSE";
     cout << text2 << endl;
-    string encrypted_text2 = encrypt(text2, 14);
+    string encrypted_text2 = beaufortCipher(text2, 14);
     cout << encrypted_text2 << endl;
 }
