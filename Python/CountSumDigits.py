cc013dd99da880c18810002b3cd7c9c4c1be197d
Konrad <konradmalenczak@gmail.com>
2025-03-13 22:13:20 +0100

    Updated CountSumDigits algorithm.

--- Python/CountSumDigits.py
+++ Python/CountSumDigits.py
@@ -5,6 +5,9 @@ def count_digits(number: int) -> int:
         number //= 10
     return count
 
+print(count_digits(12345))
+print(count_digits(65432))
+
 def sum_digits(number: int) -> int:
     sum = 0
     while number > 0:
@@ -12,22 +15,21 @@ def sum_digits(number: int) -> int:
         number //= 10
     return sum
 
+print(sum_digits(12345))
+print(sum_digits(65432))
+
 def count_digits_recursive(number: int) -> int:
     if number == 0:
         return 0
     return 1 + count_digits_recursive(number // 10)
+
+print(count_digits_recursive(12345))
+print(count_digits_recursive(65432))
 
 def sum_digits_recursive(number: int) -> int:
     if number == 0:
         return 0
     return (number % 10) + sum_digits_recursive(number // 10)
 
-print(count_digits(12345))
-print(count_digits(65432))
-print(sum_digits(12345))
-print(sum_digits(65432))
-
-print(count_digits_recursive(12345))
-print(count_digits_recursive(65432))
 print(sum_digits_recursive(12345))
 print(sum_digits_recursive(65432))
