aa5144ecffd5af2f9031a7b03a17dfc4c50a8731
Konrad <konradmalenczak@gmail.com>
2025-03-13 21:47:57 +0100

    Added recursive version of DecimalToBinaryFraction algorithm.

--- CPP/DecimalToBinaryFraction.cpp
+++ CPP/DecimalToBinaryFraction.cpp
@@ -17,8 +17,24 @@ string decimalToBinaryFraction(double fraction, int limit) {
     return binary_fractional_part;
 }
 
+string decimalToBinaryFractionRecursive(double fraction, int limit, string binary_fractional_part = "0.") {
+    if (limit == 0 || fraction == 0.0) {
+        return binary_fractional_part;
+    }
+
+    fraction *= 2;
+    if (fraction >= 1) {
+        return decimalToBinaryFractionRecursive(fraction - 1, limit - 1, binary_fractional_part + "1");
+    } else {
+        return decimalToBinaryFractionRecursive(fraction, limit - 1, binary_fractional_part + "0");
+    }
+}
+
 int main() {
     double fraction = 0.8;
-    string binary_number = decimalToBinaryFraction(fraction, 10);
+    int limit = 10;
+    string binary_number = decimalToBinaryFraction(fraction, limit);
     cout << binary_number << endl;
+    string binary_number_recursive = decimalToBinaryFractionRecursive(fraction, limit);
+    cout << binary_number_recursive << endl;
 }
--- Python/DecimalToBinaryFraction.py
+++ Python/DecimalToBinaryFraction.py
@@ -10,6 +10,19 @@ def decimal_to_binary_fraction(fraction: float, limit: int) -> str:
         limit -= 1
     return binary_fractional_part
 
+def decimal_to_binary_fraction_recursive(fraction: float, limit: int, binary_fractional_part: str = "0.") -> str:
+    if limit == 0 or fraction == 0.0:
+        return binary_fractional_part
+
+    fraction *= 2
+    if fraction >= 1:
+        return decimal_to_binary_fraction_recursive(fraction - 1, limit - 1, binary_fractional_part + "1")
+    else:
+        return decimal_to_binary_fraction_recursive(fraction, limit - 1, binary_fractional_part + "0")
+
 fraction = 0.8
-binary_number = decimal_to_binary_fraction(fraction, 10)
+limit = 10
+binary_number = decimal_to_binary_fraction(fraction, limit)
 print(binary_number)
+binary_number_recursive = decimal_to_binary_fraction(fraction, limit)
+print(binary_number_recursive)
