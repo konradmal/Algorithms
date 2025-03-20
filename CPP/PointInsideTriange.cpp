49387d3dab021c096d806f38774ddb34672e19fe
Konrad <konradmalenczak@gmail.com>
2025-03-20 18:10:03 +0100

    Added PointInsideTriangle algorithm.

---
+++ CPP/PointInsideTriange.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+bool pointInsideTriangle(double Ax, double Ay, double Bx, double By, double Cx, double Cy, double Dx, double Dy) {
+    if (((Dy - Ay) * (Bx - Ax) > (By - Ay) * (Dx - Ax) &&
+        (Dy - By) * (Cx - Bx) > (Cy - By) * (Dx - Bx) &&
+        (Dy - Cy) * (Ax - Cx) > (Ay - Cy) * (Dx - Cx)) || 
+        ((Dy - Ay) * (Bx - Ax) < (By - Ay) * (Dx - Ax) &&
+        (Dy - By) * (Cx - Bx) < (Cy - By) * (Dx - Bx) &&
+        (Dy - Cy) * (Ax - Cx) < (Ay - Cy) * (Dx - Cx))){
+    return true;
+    }
+        
+    return false;
+}
+
+int main() {
+    cout << pointInsideTriangle(0, 0, 4, 0, 2, 4, 2, 2) << endl;
+    cout << pointInsideTriangle(0, 0, 4, 0, 2, 4, 4, 4) << endl;
+}
---
+++ Python/PointInsideTriange.py
@@ -0,0 +1,12 @@
+def point_inside_triangle(Ax, Ay, Bx, By, Cx, Cy, Dx, Dy):
+    if (((Dy - Ay) * (Bx - Ax) > (By - Ay) * (Dx - Ax) and
+         (Dy - By) * (Cx - Bx) > (Cy - By) * (Dx - Bx) and
+         (Dy - Cy) * (Ax - Cx) > (Ay - Cy) * (Dx - Cx)) or
+        ((Dy - Ay) * (Bx - Ax) < (By - Ay) * (Dx - Ax) and
+         (Dy - By) * (Cx - Bx) < (Cy - By) * (Dx - Bx) and
+         (Dy - Cy) * (Ax - Cx) < (Ay - Cy) * (Dx - Cx))):
+        return True
+    return False
+
+print(point_inside_triangle(0, 0, 4, 0, 2, 4, 2, 2))
+print(point_inside_triangle(0, 0, 4, 0, 2, 4, 4, 4))
