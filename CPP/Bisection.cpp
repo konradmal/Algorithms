5b7f1a6c2593c20e144f482ac0c55108fb2d2c1a
Konrad <konradmalenczak@gmail.com>
2025-03-28 22:51:38 +0100

    Added time complexity for Bisection algorithm.

--- CPP/Bisection.cpp
+++ CPP/Bisection.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 //O(1)
 double f(double x){
-	return 2*x*x*x-2*x-8;	//1.7963
+    return 2*x*x*x-2*x-8;	//1.7963
 }
 
 //(logn)
@@ -42,6 +42,6 @@ double bisectionMethodRecursive(double start, double end, double tolerance) {
 }
 
 int main() {
-	cout << bisectionMethodIterative(0, 5, 0.0001) << endl;
-	cout << bisectionMethodRecursive(0, 5, 0.0001) << endl;
+    cout << bisectionMethodIterative(0, 5, 0.0001) << endl;
+    cout << bisectionMethodRecursive(0, 5, 0.0001) << endl;
 }
