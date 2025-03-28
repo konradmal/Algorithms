0cd5d76a22e040f6ade31bfbcf34552466acb25d
Konrad <konradmalenczak@gmail.com>
2025-03-28 22:56:11 +0100

    Added time complexity for Fibonacci algorithm.

--- CPP/Fibonacci.cpp
+++ CPP/Fibonacci.cpp
@@ -9,37 +9,37 @@ int fibonacciIterative(int n){
 	}
     if (n == 0){
         return 0;
-	}
-	int a = 1;
-	int b = 1;
-	for(int i=1; i<n; i++){
-		int tmp = a;
-		a = b;
-		b = tmp + b;
-	}
-	return a;
+    }
+    int a = 1;
+    int b = 1;
+    for(int i=1; i<n; i++){
+        int tmp = a;
+        a = b;
+        b = tmp + b;
+    }
+    return a;
 }
 
 //O(2^n)
 int fibonacciRecursive(int n){
     if (n <= -1){
-		return -1;
-	}
-	if (n == 0){
-		return 0;
-	}
-	if (n <= 2){
-		return 1;
-	}
-	return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
+        return -1;
+    }
+    if (n == 0){
+        return 0;
+    }
+    if (n <= 2){
+        return 1;
+    }
+    return fibonacciRecursive(n-1) + fibonacciRecursive(n-2);
 }
 
 int main() {
-	cout << fibonacciIterative(10) << endl;
-	cout << fibonacciIterative(33) << endl;
-	cout << fibonacciIterative(34) << endl;
+    cout << fibonacciIterative(10) << endl;
+    cout << fibonacciIterative(33) << endl;
+    cout << fibonacciIterative(34) << endl;
 
-	cout << fibonacciRecursive(10) << endl;
-	cout << fibonacciRecursive(33) << endl;
-	cout << fibonacciRecursive(34) << endl;
+    cout << fibonacciRecursive(10) << endl;
+    cout << fibonacciRecursive(33) << endl;
+    cout << fibonacciRecursive(34) << endl;
 }
