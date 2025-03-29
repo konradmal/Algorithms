7368dcf3f87ae08602a80c8631628f71ba18acf9
Konrad <konradmalenczak@gmail.com>
2025-03-29 15:51:23 +0100

    Added time complexity for Hanoi algorithm.

--- CPP/Hanoi.cpp
+++ CPP/Hanoi.cpp
@@ -3,14 +3,14 @@ using namespace std;
 
 //O(2^n)
 void hanoi(int n, string A, string B, string C){
-	if(n > 0){
-		hanoi(n-1, A, C, B);
-		cout << A << " -> " << C << endl;
-		hanoi(n-1, B, A, C);
-	}
+    if(n > 0){
+        hanoi(n-1, A, C, B);
+        cout << A << " -> " << C << endl;
+        hanoi(n-1, B, A, C);
+    }
 }
 
 int main() {
-	hanoi(3, "A", "B", "C");
-	hanoi(4, "A", "B", "C");
+    hanoi(3, "A", "B", "C");
+    hanoi(4, "A", "B", "C");
 }
