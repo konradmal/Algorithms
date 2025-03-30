c67bdc071673c9bf0088e40bc6e316c8680d1a0e
Konrad <konradmalenczak@gmail.com>
2025-03-30 10:49:09 +0200

    Added time complexity for MinMax algorithm.

--- Python/MinMax.py
+++ Python/MinMax.py
@@ -9,6 +9,5 @@ def min_max(values: list) -> tuple:
             min_value = value
     return (min_value, max_value)
 
-# Przykład użycia
 numbers = [22, 34, 12, 9, 55, 33, 23]
 print("Minimum:", min_max(numbers)[0], "Maximum:", min_max(numbers)[1])
